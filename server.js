b044c758cf626d2942b2aa61798f7f4fb10df927
Abid F <r.rumah112@gmail.com>
2020-11-06 05:38:21 +0700

    removing require(dotenv) in server.js because heroku deploy error, maybe because i declare require(dotenv) in the src child again

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use("/src/uploads/literatures", express.static("src/uploads/literatures"));
 
 app.use("/api/v1/", router); //grouping
 
-require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
