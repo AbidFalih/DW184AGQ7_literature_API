42b572afe796d68317f8e5b70a93b454eba04bdc
Abid F <r.rumah112@gmail.com>
2020-11-08 09:19:01 +0700

    change user assosiation (adding foreign key)

--- models/user.js
+++ models/user.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       User.hasMany(models.Literature, {
         as: "literatures",
+        foreignKey: "userId",
       });
 
       // User.belongsToMany(models.Literature, {
